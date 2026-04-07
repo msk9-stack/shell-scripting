7eee3239b6c11ad17b10666467dcd2930ddf11e0
Krish <msk972645@gmail.com>
2026-04-07 19:23:47 +0530

    Cricketers communication

--- 03-variables.sh
+++ 03-variables.sh
@@ -2,13 +2,13 @@
 
 # This script demonstrates the use of variables in shell scripting
 
-START-TIME=$(date)
+START-TIME=$(date +%s)
 
 echo "This is the start time : ${START-TIME} from now"
 
 # 10 scripts are executed here
 
-END-TIME=$(date)
+END-TIME=$(date +%s)
 echo "This is the end time : ${END-TIME} till now"
 
 TOTAL-DURATION=$((END-TIME - START-TIME))
