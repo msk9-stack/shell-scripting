0ecdcf2e3ae5d74d83fa9e5de7dc2b0912551072
Krish <msk972645@gmail.com>
2026-04-07 17:55:56 +0530

    Cricketers communication

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -2,11 +2,11 @@
 
 # converasion between two cricketers
 
-cricketer1=Virat
-cricketer2=Rohit
+cricketer1=tilak
+cricketer2=rinku
 
-echo "Virat: Hello, Rohit! How are you?"
-echo "Rohit: Hi, Virat! I'm doing well, thanks. How about you?"
-echo "Virat: I'm good too. Just preparing for the upcoming match."
-echo "Rohit: That's great! I'm also getting ready. We need to work on our strategies."
-echo "Virat: Absolutely! We should discuss our game plan and practice together."
+echo "${cricketer1}: Hello, ${cricketer2}! How are you?"
+echo "${cricketer2}: Hi, ${cricketer1}! I'm doing well, thanks. How about you?"
+echo "${cricketer1}: I'm good too. Just preparing for the upcoming match."
+echo "${cricketer2}: That's great! I'm also getting ready. We need to work on our strategies."
+echo "${cricketer1}: Absolutely! We should discuss our game plan and practice together."
