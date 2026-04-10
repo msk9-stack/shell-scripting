f70a37b70a299551dbbde30c07ca74fffeece30f
Krish <msk972645@gmail.com>
2026-04-10 19:13:18 +0530

    new variable

--- 06-mathematicOp.sh
+++ 06-mathematicOp.sh
@@ -5,7 +5,7 @@
 kohli_Test_centries=$(32)
 kohli_oneday_centries=$(54)
 
-kohli_total_centuries=$(($kohli_Test_centries + $kohli_oneday_centries))
+kohli_total_centuries=$(($kohli_Test_centries+$kohli_oneday_centries))
 
-echo "total centuries of kohli is: ${kohli_total_centuries}"
+echo "total centuries of kohli is: $kohli_total_centuries"
 
