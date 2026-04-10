ffdadd85ab74c506f1b931831d8e82b3ec210303
Krish <msk972645@gmail.com>
2026-04-10 13:35:52 +0530

    new variable

--- 04-variables.sh
+++ 04-variables.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-DATE=$(date +%F)
+# Total duration of the task:
+
+START_TIME=$(date +%s)
+
+sleep 5
+
+END_TIME=$(date +%s)
+
+TOTAL_DURATION=$(($START_TIME - $END_TIME))
 
 echo "today's date is: $DATE" 
