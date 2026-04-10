494b77a26499132177ccdeb6c936dbbfea0c703c
Krish <msk972645@gmail.com>
2026-04-10 18:12:41 +0530

    new variable

---
+++ 05-special_variables.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Player names
+
+echo "who are the players: $@"
+
+echo "who are the captains: $*"
+
+# coach name
+
+echo "who is the coach: $USER"
+
+echo "where is the match: $PWD"
+
+echo "whats the script name: $0"
