0914075257528ccbbb046b93ea5bfa20ae189ad4
Krish <msk972645@gmail.com>
2026-04-13 17:59:37 +0530

    conditions

--- 08.conditions.sh
+++ 08.conditions.sh
@@ -2,10 +2,12 @@
 
 VALUE=$1
 
-if [ "$VALUE" -lt 10 ]; then
+if [ $VALUE -lt 10 ]; then
     echo " the value $VALUE is less than 10"
+elif [ $VALUE -eq 10]; then
+    echo " the value $VALUE is equal o 10"
 else
-    echo "the value $VALUE is greter than or equal to 10"
+    echo "the value $VALUE is greter than to 10"
 fi
 
 # -lt is less than
