fb072322278f5522a1812db87f34a0aebadc3e54
Krish <msk972645@gmail.com>
2026-04-13 18:14:45 +0530

    conditions

--- 09.conditions2.sh
+++ 09.conditions2.sh
@@ -6,7 +6,7 @@ echo "enter the value:"
 
 read VALUE
 
-if [ $(($VALUE % 2)) -eq 0 ]; then
+if [ $(($VALUE + 2)) -eq 12 ]; then
     echo "the value $VALUE is even"
 else
     echo "the value $VALUE is odd"
