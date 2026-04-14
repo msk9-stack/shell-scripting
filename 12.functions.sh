0bec03ef7941f380f5f57020fb6b5952d3578f47
Krish <msk972645@gmail.com>
2026-04-14 19:24:28 +0530

    scripts

--- 12.functions.sh
+++ 12.functions.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-name(){
-	echo "welcome to devops"
+USERID=$(id -u)
+
+if [ $USERID -ne 0]: then
+	echo "ERROR:: this script need root user to execute"
+	exit 1
+fi
+
+script( $1 -ne 0 ){
+	echo "ERROR:: script failed to run $2"
+	exit 1
+else
+	echo "SUCCESS:: installed successfully $2"
 }
 
-name 
+dnf install mysql -y
+script $? "mysql"
+
+dnf install python3 -y
+script $? "python3"
