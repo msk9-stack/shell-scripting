1e5157386f0ff693d225a9c3ccb8a24be814a824
Krish <msk972645@gmail.com>
2026-04-14 19:33:00 +0530

    scripts

--- 12.functions.sh
+++ 12.functions.sh
@@ -7,11 +7,13 @@ if [ $USERID -ne 0 ]; then
 	exit 1
 fi
 
-script ( $1 -ne 0 ){
-	echo "ERROR:: script failed to run $2"
+script(){
+if [ $1 -ne 0 ]; then
+    echo "ERROR:: script failed to run $2"
 	exit 1
 else
 	echo "SUCCESS:: installed successfully $2"
+fi
 }
 
 dnf install mysql -y
