2fa789b745e563eb95de1267611113d767878126
Krish <msk972645@gmail.com>
2026-04-14 16:08:45 +0530

    scripts

--- 10.package_install.sh
+++ 10.package_install.sh
@@ -6,10 +6,10 @@ if [ $USER -ne 0 ]; then
     echo " ERROR:: login as root user $USER"
 fi
 
-dnf remove mysql -y
+dnf install mysql -y
 
 if [ $? -ne 0 ]; then
     echo "ERROR:: failed to install mysql"
 else
-    echo "SUCCESS:: mysql removed successfully"
+    echo "SUCCESS:: mysql installed successfully"
 fi
