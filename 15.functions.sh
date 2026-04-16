6609c3943a4d8fd65b262817d3aca9d7b738b0ed
Krish <msk972645@gmail.com>
2026-04-16 17:54:37 +0530

    variables used as (-eq 0)

--- 15.functions.sh
+++ 15.functions.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+
 echo "this script will install multiple serverce at once"
 
 USERID=$(id -u)
 
 if [ $USERID -eq 0 ]; then
-	echo "SUCCESS:: verified the root user successfully, about to install the script"
+	echo -e "$R SUCCESS:: verified the root user $Y successfully, about to install the script"
 else
-	echo "ERROR:: only root user can run this script, login as root user"
+	echo -e "$G ERROR:: only root user can run this script, login as root user"
     exit 1
 fi
 
 VALIDATION(){
 	if [ $1 -eq 0 ]; then
-		echo -e "the package $2 installed \e[33m successfully"
+		echo "the package $2 installed $Y successfully"
 	else
-		echo "the package $2 failed to install"
+		echo "the package $2 $G failed to install"
 	fi
 }
 
